08c1e78ba5fd9dc697f00f2e35300b819e9b9740
machadomatt <matheusmacsyn@gmail.com>
2021-06-20 01:54:49 -0300

    Create Category and Post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Category;
+use App\Post;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    $title = $faker->paragraph(1);
+    $userID = User::pluck('id')->random();
+    $categoryID = Category::pluck('id')->random();
+
+    return [
+        'slug' => str_slug($title),
+        'title' => $title,
+        'excerpt' => $faker->paragraph(2),
+        'reading_time' => $faker->numberBetween(1, 12),
+        'content' => $faker->paragraph(20),
+        'author' => $userID,
+        'category' => $categoryID,
+        'published_at' => $faker->dateTimeBetween('-2 months', 'now')
+    ];
+});
