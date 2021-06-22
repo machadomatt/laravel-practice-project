53e25b8071c6c417bba7fc5b2e79ed68894fa7e4
machadomatt <matheusmacsyn@gmail.com>
2021-06-21 23:28:52 -0300

    Add about images

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -22,7 +22,7 @@
         <div class="container px-5 my-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0"
-                        src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                        src="{{ Storage::url(Cropper::thumb('images/about-1.jpg', 600, 400)) }}" alt="..." /></div>
                 <div class="col-lg-6">
                     <h2 class="fw-bolder">Our founding</h2>
                     <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
@@ -37,7 +37,7 @@
         <div class="container px-5 my-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0"
-                        src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
+                        src="{{ Storage::url(Cropper::thumb('images/about-2.jpg', 600, 400)) }}" alt="..." /></div>
                 <div class="col-lg-6">
                     <h2 class="fw-bolder">Growth &amp; beyond</h2>
                     <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
@@ -57,32 +57,32 @@
             <div class="row gx-5 row-cols-1 row-cols-sm-2 row-cols-xl-4 justify-content-center">
                 <div class="col mb-5 mb-5 mb-xl-0">
                     <div class="text-center">
-                        <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d"
-                            alt="..." />
+                        <img class="img-fluid rounded-circle mb-4 px-4"
+                            src="{{ Storage::url(Cropper::thumb('images/profile-image-1.jpg', 150, 150)) }}" alt="..." />
                         <h5 class="fw-bolder">Ibbie Eckart</h5>
                         <div class="fst-italic text-muted">Founder &amp; CEO</div>
                     </div>
                 </div>
                 <div class="col mb-5 mb-5 mb-xl-0">
                     <div class="text-center">
-                        <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d"
-                            alt="..." />
+                        <img class="img-fluid rounded-circle mb-4 px-4"
+                            src="{{ Storage::url(Cropper::thumb('images/profile-image-2.jpg', 150, 150)) }}" alt="..." />
                         <h5 class="fw-bolder">Arden Vasek</h5>
                         <div class="fst-italic text-muted">CFO</div>
                     </div>
                 </div>
                 <div class="col mb-5 mb-5 mb-sm-0">
                     <div class="text-center">
-                        <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d"
-                            alt="..." />
+                        <img class="img-fluid rounded-circle mb-4 px-4"
+                            src="{{ Storage::url(Cropper::thumb('images/profile-image-3.jpg', 150, 150)) }}" alt="..." />
                         <h5 class="fw-bolder">Toribio Nerthus</h5>
                         <div class="fst-italic text-muted">Operations Manager</div>
                     </div>
                 </div>
                 <div class="col mb-5">
                     <div class="text-center">
-                        <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d"
-                            alt="..." />
+                        <img class="img-fluid rounded-circle mb-4 px-4"
+                            src="{{ Storage::url(Cropper::thumb('images/profile-image-4.jpg', 150, 150)) }}" alt="..." />
                         <h5 class="fw-bolder">Malvina Cilla</h5>
                         <div class="fst-italic text-muted">CTO</div>
                     </div>
